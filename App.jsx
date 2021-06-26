70c63ca4bbdf8cdf9e931c25c893338e5320b394
Avi2002Soy <86245437+Avi2002Soy@users.noreply.github.com>
2021-06-26 16:36:23 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Info from "./Info";
+import sytles from "./styles.css"
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Info />
+      <Footer />
+      <styles />
+    </div>
+  );
+}
+
+export default App;
---
+++ Info.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="row">
+      <div className="card">
+        <div className="note1">
+          <h1>Data Analyst Training and Internship Program(3 months)</h1>
+          <p>
+            No prerequiste required. It is a 11 weeks Internship and Training
+            Program which consisted of 30 projects The Goal of this Training and
+            Internship program is to Advance your programming skills and refine
+            your ability to work with messy, complex datasets. You’ll learn to
+            manipulate and prepare data for analysis, and create visualizations
+            to tell your life
+          </p>
+        </div>
+      </div>
+
+      <div className="card">
+        <div className="note2">
+          <h1>Cyber Security Training and Internship Program(3 months)</h1>
+          <p>
+            It helps interns to instill in them the knowledge of the vast domain
+            of Cyber Security which includes skills in defending computers,
+            servers, mobile developers, managers, engineers, and even private
+            citizens will benefit from this learning experience. The learning
+            process will be kept simple and accurate from pillar to post.There
+            is No Prerequisites required
+          </p>
+        </div>
+      </div>
+
+      <div className="card">
+        <div className="note3">
+          <h1>Full stack Web dev Training and Internship Program(3 months)</h1>
+          <p>
+            A graduate of this program will be able to design and build
+            databases for software applications, create and deploy
+            database-backed web APIs, and secure and manage user authentication
+            and access control for an application backend. They will be able to
+            build production level frontend according to the industrial
+            standards. Students will also learn how to deploy a the entire web
+            application to the cloud using Docker and Kubernetes and how to
+            maintain and scale them.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Info
